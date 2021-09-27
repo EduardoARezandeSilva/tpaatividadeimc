9e3a5d65926bf5caf267035080856c4c491b69c4
EduardoARezandeSilva <80038803+EduardoARezandeSilva@users.noreply.github.com>
2021-09-27 08:07:01 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,48 +1,39 @@
 function calcular() 
 {
-    window.alert("Preencha todos os campos");
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const resulatdo = document.getElementById('resultado');
 
     if (nome != "" || altura != "" || peso != "")
-    {
-        window.alert("Preencha todos os campos");
-        
-        const imc = (peso / (altura * altura)).toFixed(1)
+    {        
+        const imc = (peso / (altura**2)).toFixed(1)
 
         const peso_min = (18.6 * (altura**2)).toFixed(1)
         const peso_max = (24.9 * (altura**2)).toFixed(1)
         
         if(imc <= 18.5)
         {
-            window.alert("Preencha todos os campos");
             resulatdo.textContent = nome + " pelo seus dados de altura " + altura + "M e peso " + peso + "KG com isso seu imc e " + imc + " e você esta abaixo do peso!!! Sua faixa de peso ideal está entre " + peso_min + "KG e " + peso_max + "KG!!!" 
         }
         else if (imc <= 24.9)
         {
-            window.alert("Preencha todos os campos");
             resulatdo.textContent = nome + " pelo seus dados de altura " + altura + "M e peso " + peso + "KG com isso seu imc e " + imc + " e você esta no peso normal!!! Sua faixa de peso ideal está entre " + peso_min + "KG e " + peso_max + "KG!!!" 
         }
         else if (imc <= 29.9)
         {
-            window.alert("Preencha todos os campos");
             resulatdo.textContent = nome + " pelo seus dados de altura " + altura + "M e peso " + peso + "KG com isso seu imc e " + imc + " e você esta com sobrepeso!!! Sua faixa de peso ideal está entre " + peso_min + "KG e " + peso_max + "KG!!!" 
         }
         else if (imc <= 34.9)
         {
-            window.alert("Preencha todos os campos");
             resulatdo.textContent = nome + " pelo seus dados de altura " + altura + "M e peso " + peso + "KG com isso seu imc e " + imc + " e você esta com obesidade I !!! Sua faixa de peso ideal está entre " + peso_min + "KG e " + peso_max + "KG!!!" 
         }
         else if (imc <= 39.9)
         {
-            window.alert("Preencha todos os campos");
             resulatdo.textContent = nome + " pelo seus dados de altura " + altura + "M e peso " + peso + "KG com isso seu imc e " + imc + " e você esta com obesidade II !!! Sua faixa de peso ideal está entre " + peso_min + "KG e " + peso_max + "KG!!!" 
         }
         else
         {
-            window.alert("Preencha todos os campos");
             resulatdo.textContent = nome + " pelo seus dados de altura " + altura + "M e peso " + peso + "KG com issoseu imc e " + imc + " e você esta com obesidade III !!! Sua faixa de peso ideal está entre " + peso_min + "KG e " + peso_max + "KG!!!" 
         }
     }
